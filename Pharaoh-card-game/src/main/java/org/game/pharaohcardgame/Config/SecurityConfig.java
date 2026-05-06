cc7bfde0f5ad4268f0106c540ed4f5f1227f8ed7
shellking41 <b.szabiszabi@gmail.com>
2026-05-06 18:32:10 +0200

    init

--- Pharaoh-card-game/src/main/java/org/game/pharaohcardgame/Config/SecurityConfig.java
+++ Pharaoh-card-game/src/main/java/org/game/pharaohcardgame/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.game.pharaohcardgame.Authentication.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -34,6 +35,8 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+		                .requestMatchers(HttpMethod.OPTIONS).permitAll()
+
                         // Nyilvános endpointok
                         .requestMatchers("/auth/generate/temp-user-token", "/room/join-room-request","/auth/register", "/auth/login","/user/current-status","/auth/**")
                         .permitAll()
